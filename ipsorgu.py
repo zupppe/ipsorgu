9794517b19e22cc1425391ac09500fcd36d81fa2
vHzEren <131760599+vHzEren@users.noreply.github.com>
2024-10-27 22:01:59 +0300

    Update ipsorgu.py

--- ipsorgu.py
+++ ipsorgu.py
@@ -1,17 +1,38 @@
 import requests
+import colorama
+from colorama import Fore
+import os
+import time
+
+os.system("@echo off")
+temizle = lambda: os.system("cls")
+baslat = lambda: os.system("py ipsorgu.py")
 
 def check(): 
     r = requests.get("https://ipinfo.io/") 
     if r.status_code == 200: 
-        print("\n[+] Sunucu Çevrimiçi!\n") 
+        print(Fore.GREEN + "\n[+] Sunucu Çevrimiçi!\n") 
     else:
-        print("\n[!] Sunucu Çevrimdışı!\n") 
+        print(Fore.RED + "\n[!] Sunucu Çevrimdışı!\n") 
         exit() 
 
-ip = input("Lütfen hedef ip giriniz: ") 
+ip = input(Fore.BLUE + "Lütfen hedef ip giriniz: ") 
 
 check()
 
 ip_cikti = requests.get("https://ipinfo.io/widget/demo/{}".format(ip)).text
+print(str(ip_cikti))
 
-print(ip_cikti)
+geri_don = input("{}Yeni Sorgu İçin {}+{}, Ana Menü İçin {}- {}Tuşlayınız {}+{}/{}-{}: {}".format(Fore.BLUE, Fore.GREEN, Fore.BLUE, Fore.RED, Fore.BLUE, Fore.GREEN, Fore.BLUE, Fore.RED, Fore.BLUE, Fore.RESET))
+if geri_don == ("+"):
+    temizle()
+    baslat()
+if geri_don == ("-"):
+    temizle()
+    os.chdir("..")
+    os.system("py tools.py")
+if geri_don != ("+","-"):
+    temizle()
+    print(Fore.LIGHTRED_EX + "Hata Tespit Edildi En Başa Yönlendiriliyorsunuz... @zupppe")
+    time.sleep(3)
+    baslat()
